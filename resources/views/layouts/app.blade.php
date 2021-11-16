d8645d2cbc1745f987c79221a929a8a7c05bc050
Ayan <lennon06@inbox.ru>
2021-11-16 22:11:04 +0600

    favicon added'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+	<link rel="icon" href="{{asset('img/favicon.png')}}" type="image/png">
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
